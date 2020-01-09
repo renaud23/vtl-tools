785a70ac8919b54e65d71fb13aab3e1a33590f4e
Renaud Genevois <renaud.genevois@insee.fr>
2020-01-09 13:35:31 +0100

    editor tokens

--- vtl-editor/src/editor/components/tokens-layer/row.js
+++ vtl-editor/src/editor/components/tokens-layer/row.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Token from "./token";
 
+/**
+ *
+ * @param {*} rowStart
+ * @param {*} valueRow
+ * @param {*} token
+ */
+const computeTokenValue = (rowStart, valueRow, token) => {
+  const { start, stop, value } = token;
+  return value.substr(
+    Math.max(0, rowStart - start),
+    Math.min(valueRow.length, stop - start)
+  );
+};
+
 function Row({ value, start, tokens }) {
   return (
     <div className="row">
       {tokens.map((t, i) => (
-        <Token key={i} value={t.value} className={t.className} />
+        <Token
+          key={i}
+          value={computeTokenValue(start, value, t)}
+          className={t.className}
+        />
       ))}
     </div>
   );
