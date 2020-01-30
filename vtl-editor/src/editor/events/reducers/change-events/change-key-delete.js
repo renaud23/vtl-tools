bdbb237df3f17e987dc9fc920702b8380df1c524
Renaud Genevois <renaud.genevois@insee.fr>
2020-01-30 17:05:11 +0100

    bug ket delete end line

--- vtl-editor/src/editor/events/reducers/change-events/change-key-delete.js
+++ vtl-editor/src/editor/events/reducers/change-events/change-key-delete.js
@@ -1,14 +1,16 @@
 import changeDeleteSelection from "./change-delete-selection";
 import { computeSourcePosition } from "../../../tools";
+import { getLineSeparator } from "../../../tools";
 
 function changeKeyDelete(state) {
   const { anchor, extent, cursor, lines, source } = state;
   if (anchor && extent) {
     return changeDeleteSelection(state);
   }
-  const [{ pos }] = computeSourcePosition(lines, cursor);
-  if (pos < source.length - 1) {
-    const nextSource = `${source.substr(0, pos)}${source.substr(pos + 1)}`;
+  const [{ pos, row, index }] = computeSourcePosition(lines, cursor);
+  if (pos < source.length) {
+    const length = index === lines[row].length ? getLineSeparator().length : 1;
+    const nextSource = `${source.substr(0, pos)}${source.substr(pos + length)}`;
 
     return {
       ...state,
