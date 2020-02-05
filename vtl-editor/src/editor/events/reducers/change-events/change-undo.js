09702a2c357ccb5e0b2e36d0cf22cdf796be6ba8
Renaud Genevois <renaud.genevois@insee.fr>
2020-02-05 13:40:46 +0100

    bug ctrl|z cursor on new line

--- vtl-editor/src/editor/events/reducers/change-events/change-undo.js
+++ vtl-editor/src/editor/events/reducers/change-events/change-undo.js
@@ -1,40 +1,14 @@
 import { DELETE_FRAGMENT, INSERT_FRAGMENT } from "../source-events";
 import { getLineSeparator } from "../../../tools/split-lines";
 
-// function computeInsertFragment(source, diff) {
-//   const { start, fragment } = diff;
-//   return `${source.substr(0, start)}${fragment}${source.substr(start)}`;
-// }
-
-// function computeDeleteFragment(source, diff) {
-//   const { start, stop } = diff;
-//   return `${source.substr(0, start)}${source.substr(stop + 1)}`;
-// }
-
-// function computeDiff(origin, diff) {
-//   const next = diff.reduce((source, one, i) => {
-//     const { type, payload } = one;
-//     switch (type) {
-//       case DELETE_FRAGMENT:
-//         return computeDeleteFragment(source, payload);
-//       case INSERT_FRAGMENT:
-//         return computeInsertFragment(source, payload);
-//       default:
-//         return source;
-//     }
-//   }, origin);
-
-//   return next;
-// }
-
 function getCursor(source, pos) {
   return source.split(getLineSeparator()).reduce(
     ({ row, index, acc }, l, i) => {
-      const nextAcc = acc + l.length + getLineSeparator().length;
+      const nextAcc = acc + l.length;
       if (acc && pos >= acc && pos <= nextAcc) {
         return { row: i, index: pos - acc };
       }
-      return { row, index, acc: nextAcc };
+      return { row, index, acc: nextAcc + getLineSeparator().length };
     },
     { row: 0, index: 0, acc: 0 }
   );
